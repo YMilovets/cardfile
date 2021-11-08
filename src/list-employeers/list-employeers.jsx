1c469361f9ef571721751bcd9f3ca00650c56dfd
YMilovets <y.milovets@yandex.ru>
2021-11-08 16:58:41 +0700

    Added json-file

--- src/list-employeers/list-employeers.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function ListEmployeers(props) {
-    return (
-        <ul className="list-employeers list-group m-3 flex-grow-0 flex-shrink-0">
-            {props.children}
-        </ul>
-    )
-}
--- src/message/message.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Message(props) {
-    return (
-        <p className="message m-3">
-            {props.children}
-        </p>
-    )
-}
